a7cb007ca0de1c04e098991f174842f341730297
Johny Kim <johnyworld@naver.com>
2019-09-23 14:16:08 +0900

    Add : Login with graphql

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { gql } from 'apollo-boost'
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme';
 import AppRouter from './Router';
+import { useQuery } from 'react-apollo-hooks';
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <>
-      <AppRouter isLoggedIn={false} />
-      <GlobalStyles />
-    </>
-  </ThemeProvider>
-);
+const QUERY = gql`
+  { isLoggedIn @client }
+`;
+
+export default () => {
+  const { data: { isLoggedIn }} = useQuery(QUERY);
+
+  console.log(isLoggedIn);
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
+        <AppRouter isLoggedIn={isLoggedIn} />
+        <GlobalStyles />
+      </>
+    </ThemeProvider>
+  );
+} 
