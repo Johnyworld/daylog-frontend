5fb18a7f3b46f861d58d8aae0d111f337050fa98
Johny Kim <johnyworld@naver.com>
2019-09-24 14:57:30 +0900

    Add : Theme CSS

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -2,8 +2,17 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap');
+    @import url('https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,300,400,700&display=swap');
+    @import url('https://fonts.googleapis.com/css?family=Lobster&display=swap');
+
     ${reset}
+
     * {
         box-sizing: border-box;
+    }
+
+    html, input {
+        font-family: 'Open Sans', 'Noto Sans KR', sans-serif;
     }
 `;
--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -1,5 +1,7 @@
 export default {
     c_blue: "#1a9df9",
+    c_blueBrighter1: "#61bffb",
+    c_blueBrighter2: "#94d1fc",
     c_black: "#222",
     c_red: "#eb3f3f",
     c_lightGray: "#ddd",
