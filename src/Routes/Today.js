c6980aeaaf275a2f2172d8e0fc95218fdb2769be
Johny Kim <johnyworld@naver.com>
2019-09-25 13:47:16 +0900

    Add : Today Router

--- src/Routes/Today.js
+++ src/Routes/Today.js
@@ -1,1 +1,42 @@
-export default () => 'Today';
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../Components/Header';
+import { gql } from 'apollo-boost';
+import { useQuery } from 'react-apollo-hooks';
+import Loader from '../Components/Loader';
+
+const TODAY_QUERY = gql`
+    {
+       seeTodayPosts {
+            doing {
+                name
+            }
+            likesCount
+            commentsCount
+            isLiked
+            score
+            startAt
+            endAt
+        }
+    } 
+`;
+
+const Container = styled.main`
+
+`;
+
+export default () => {
+    const { data, loading } = useQuery( TODAY_QUERY );
+
+    return (
+        <>
+            <Header page="today" />
+            { loading ? <Loader /> :
+                <Container>
+                    Loading Done
+                </Container>
+            }
+ 
+        </>
+    )
+};
