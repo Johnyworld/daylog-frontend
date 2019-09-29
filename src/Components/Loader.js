0ac99114c684ae0e9861cac9f0903d68a6eb4704
Johny Kim <johnyworld@naver.com>
2019-09-29 21:06:49 +0900

    Fix loader layout

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -2,11 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
+    position: absolute;
+    height: 100%;
+    width: 100%;
 `;
 
 const Loader = styled.div`
