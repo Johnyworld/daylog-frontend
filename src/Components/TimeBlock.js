0f5e5dcb904fcc4001c5abdd39e4797916b364c5
Johny Kim <johnyworld@naver.com>
2019-10-05 17:55:06 +0900

    Add Today Router

---
+++ src/Components/TimeBlock.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import TextRegular from './TextRegular';
+import Score from './Score';
+import SmallButton from './SmallButton';
+import Words from '../Lang/Words.json';
+import SetScore from './SetScore';
+
+const Container = styled.li`
+    position: relative;
+    display: flex;
+    padding: 0 5px;
+    width: 60vw;
+    height: 36px;
+    background-color: ${({ theme })=> theme.c_lightGrayBrighter1 };
+    &:not(:first-child) { border-top: 1px solid white; }
+    &.hour { border-top: 3px solid white; }
+    /* &.hour:first-child { border-top: 0; } */
+    &:first-child { border: 3px solid ${({ theme })=> theme.c_black }}
+    ${({ color, doing })=> {
+        if ( color && !doing ) return `
+        border-color: #0000000f !important;
+        background-color: ${color}; 
+        `
+        else if ( color ) return `background-color: ${color}; `
+    }};
+`;
+
+const Inner = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+
+    .small-button {
+        .text-regular {
+            ${({ theme })=> theme.f_small };
+        }
+        width: 30px;
+    }
+`;
+
+const TimePrint = styled.div`
+    ${({ theme })=> theme.f_regular };
+    color: ${({ theme })=> theme.c_black };
+    font-weight: 300;
+    position: absolute;
+    left: 0;
+    top: 0;
+    transform: translate( -150%, -50% );
+`;
+
+
+const TimeBlock = ({ block, doing, color, score, blocks, isLiked, likesCount, postsCount, lang }) => {
+    const [ scoreState, setScoreState ] = useState(score ? score : null);
+    const [ scorePopup, setScorePopup ] = useState(false);
+
+    const onScorePopup = () => {
+        setScorePopup(true);
+    }
+
+    const closePopup = () => {
+        setScorePopup(false);
+    }
+
+    return (
+        <Container className={ block%4 === 0 && "hour" } color={color} doing={doing} >
+            { doing && (
+                <Inner>
+                    <TextRegular string={doing} color="white" />
+                    { scoreState
+                        ? <Score score={score} size="small" color="white" /> 
+                        : <SmallButton onClick={onScorePopup} text={Words.setScore} lang={lang} color="white" />
+                    }
+                </Inner>
+            )}
+            <TimePrint>
+                { block%4 === 0 && ( block/4 > 9 ? block/4 : `0${block/4}` )}
+            </TimePrint>
+            { scorePopup && 
+                <SetScore 
+                    doing={doing}
+                    closePopup={closePopup}
+                    lang={lang}
+                    blocks={blocks}
+                    color={color}
+                    scoreState={scoreState}
+                    setScoreState={setScoreState}
+            />}
+        </Container>
+    )
+}
+
+export default TimeBlock;
--- src/Routes/Today.js
+++ src/Routes/Today.js
@@ -4,23 +4,89 @@ import { useQuery } from 'react-apollo-hooks';
 import Loader from '../Components/Loader';
 import { TODAY_QUERY, ME } from '../Router';
 import { getLang } from '../Util/Languages';
+import TimeBlock from '../Components/TimeBlock';
+import { getNowBlock, getYyyymmdd } from '../Util/Convertors';
 
 const Container = styled.main`
     ${({ theme })=> theme.mainContainer };
+    overflow-y: scroll;
+`;
+
+const TimeBlocks = styled.ul`
+    margin-left: 35px;
+    padding-top: 20vh;
 `;
 
 export default () => {
     const { data, loading } = useQuery( TODAY_QUERY );
     const { data: meData, loading: meLoading } = useQuery(ME);
 
-    const lang = getLang( meData && meData.me && !meLoading && meData.me.lang );
+    if ( !loading && data && data.seeTodayPosts && meData && meData.me && !meLoading ) {
+        const now = getNowBlock();
 
-    return (
-        <Container>
-            { loading && <Loader /> }
-            { !loading && data && data.seeTodayPosts && 
-                <p>Loading Done</p>
+        const yesterday = new Date();
+        yesterday.setDate( yesterday.getDate() -1 );
+        const yyyymmddYesterday = getYyyymmdd( yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate() );
+        
+        let blocks = []
+        for ( let i=0; i<96; i++ ) {
+            let block = now-i;
+            let isYesterday = false;
+            if ( block < 0 ) {
+                block += 96;
+                isYesterday = true;
+            } 
+            blocks = [ ...blocks, { block, isYesterday } ];
+        }
+
+        data.seeTodayPosts.forEach( post => {
+            let endAt = post.endAt;
+            if ( post.yyyymmdd === yyyymmddYesterday ) endAt -= 96;
+
+            const num = 0 + ( now - endAt );
+            blocks[num] = { ...blocks[num], ...post };
+            for ( let i=1; i<post.blocks; i++ ) {
+                blocks[num+i].doing = { color : post.doing.color }; 
             }
-        </Container>
-    ) 
+        })
+
+        // data.seeTodayPosts.forEach( post => {
+        //     let startAt = post.startAt;
+        //     if ( post.yyyymmdd === yyyymmddYesterday ) startAt -= 96;
+
+        //     const num = 95 - ( now - startAt );
+        //     blocks[num] = { ...blocks[num], ...post };
+        //     for ( let i=1; i<post.blocks; i++ ) {
+        //         blocks[num+i].doing = { color : post.doing.color }; 
+        //     }
+        // })
+
+        const lang = getLang( meData.me.lang );
+        console.log(blocks);
+        // window.scrollTo( 0, document.body.clientHeight );
+        
+        return (
+            <Container>
+                <TimeBlocks>
+                    { blocks.map((block, key) => {
+                        return (
+                            <TimeBlock 
+                                key={block.block} 
+                                block={block.block}
+                                doing={block.doing ? block.doing.name : null}
+                                color={block.doing ? block.doing.color : null}
+                                score={block.score}
+                                blocks={block.blocks}
+                                isLiked={block.isLiked}
+                                likesCount={block.likesCount}
+                                postsCount={block.postsCount}
+                                lang={lang}
+                            />
+                        )
+                    })}
+                </TimeBlocks>
+            </Container>
+        ) 
+    } else return <Loader />
+
 };
