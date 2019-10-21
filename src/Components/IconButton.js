955bb88decbe39e5a363d0d3563ec49337c95b57
Johny Kim <johnyworld@naver.com>
2019-10-21 15:28:06 +0900

    Update IconButton component
    - Add link case

--- src/Components/IconButton.js
+++ src/Components/IconButton.js
@@ -3,19 +3,31 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Icon from './Icon';
 import Theme from '../Styles/Theme';
+import { Link } from 'react-router-dom/cjs/react-router-dom';
 
 const Container = styled.button`
     width: ${({ size })=> size }px;
     height: ${({ size })=> size }px;
 `;
 
-const IconButton = ({ className, icon, size, color, onClick }) => {
+const LinkContainer = styled(Link)`
+    width: ${({ size })=> size }px;
+    height: ${({ size })=> size }px; 
+`;
+
+const IconButton = ({ className, icon, size, color, onClick, to }) => {
 
     let sizeNum;
     if ( size === "small" ) sizeNum = Theme.size_iconSmall;
     if ( size === "medium" ) sizeNum = Theme.size_iconMedium;
 
     return (
+        to
+        ?
+        <LinkContainer to={to} className={className} size={sizeNum} onClick={onClick} >
+            <Icon icon={icon} size={size} color={color} /> 
+        </LinkContainer>
+        :
         <Container className={className} size={sizeNum} onClick={onClick} >
             <Icon icon={icon} size={size} color={color} />
         </Container>
